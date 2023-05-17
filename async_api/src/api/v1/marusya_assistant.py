ff83e4e7a6775fbb08cd493381a0a4e9302a5298
kaluzhinas <kaluzhinas@gmail.com>
2023-05-17 23:39:42 +0700

    Add search info about film and context command

--- async_api/src/api/v1/marusya_assistant.py
+++ async_api/src/api/v1/marusya_assistant.py
@@ -6,6 +6,7 @@ from fastapi import APIRouter, Depends, Request
 from urllib.parse import urljoin, urlencode
 from .voice_command.comand import text_commands
 import aiohttp
+import json
 
 
 from .deserializers.marusya_deserializer import MarusyaRequestModel
@@ -16,19 +17,20 @@ router = APIRouter()
 
 
 @router.post("/marusya",response_model= MarusyaResponseModel)
-
 async def get_data_assistant(requestMarusya: MarusyaRequestModel,
                              request: Request):
-    print(request.base_url)
+
     session_id = requestMarusya.session.session_id
     user_id = requestMarusya.session.user_id
     message_id = requestMarusya.session.message_id
-
+    state: dict = {}
     last_state = await redis.redis.get(session_id)
 
-    await redis.redis.setex(session_id,
-                            settings.cache_expires,
-                            str({'aaa':1}))
+    # Получаем последние данные которые запрашивали.
+    # Тк данные хранятся на русском делаем манипуляции для преобразования его в JSON 
+    if last_state:
+        state = json.loads(last_state.decode('utf8').replace("\'", "\""))
+
 
     resp = MarusyaResponseModel(
                 response=ResponseMarusya(
@@ -40,27 +42,49 @@ async def get_data_assistant(requestMarusya: MarusyaRequestModel,
                     )
                 )
     
-
+    # # Реакция на приветствие
     if requestMarusya.session.new:
         resp.response.text = text_commands.welcome 
-    
+    # Реакция на просьбу показать фильм
     if _check_command(requestMarusya.request.command, text_commands.film):
-        url = f'{request.base_url}/api/v1/films/search'
-        params = {
-            "page[size]": 1,
-            "page[number]": 1,
-            "sort": "-imdb_rating"
-        }
-        data_from_es = await _get_films(url=url, params=params)[0]
-        resp.response.text = 'что то про фильм'
-                
-    if _check_command(requestMarusya.request.command, text_commands.author):
-        resp.response.text = 'Автор'
+        msg = ''
+        try:
+            data_from_es = await _get_films(request=request,
+                                            phrase=requestMarusya.request.command)
+            msg = data_from_es.get('title')
+            await redis.redis.setex(session_id, settings.cache_expires, str(data_from_es))
+        except Exception as e:    
+            msg = 'Я не смогла найти данный фильм'
+
+        resp.response.text = msg
+        
+    # Реакция на просьбу получить информацию о жанре в текущем фильме
+    if _check_command(requestMarusya.request.command, text_commands.context_film_to_genre):
+        if state:
+            resp.response.text = ', '.join(state.get('genre'))
+         #TODO: тут как-то надо обработать о том что делать если мы не знаем какой фильм запрашивался ранее
+    
+    # Реакция на просьбу получить информацию об описании фильма
+    if _check_command(requestMarusya.request.command, text_commands.context_film_to_decription):
+        if state:
+            resp.response.text = state.get('description', 'Я не смоглда найти информацию о чем фильм')
+
+    # Реакция на просьбу получить информацию об актерах в фильме
+    if _check_command(requestMarusya.request.command, text_commands.context_film_to_actors):
+        if state and state.get('actors'):
+            resp.response.text = ', '.join([c.get('name') for c in state.get('actors')])
+
+
+    # Реакция на корректное завершение работы Маруси
+    if _check_command(requestMarusya.request.command, text_commands.end):
+        resp.response.text = 'Рада помочь!'
+        resp.response.end_session=True
 
                 
     return dict(resp)
 
 
+# TODO:  Вспамогательные функции вынести в отдельный модуль.
 
 def _check_command(text_request: str, command: tuple) -> bool:
     for phrase in command:
@@ -68,13 +92,27 @@ def _check_command(text_request: str, command: tuple) -> bool:
             return True
         return False
     
+async def _get_films(request: Request, phrase: str) -> dict:
+    search_film_params = {
+            "page[size]": 1,
+            "page[number]": 1,
+            "sort": "-imdb_rating",
+            "query": phrase
+        }
 
-async def _get_films(url: str, params: dict) -> dict:
+    film_id = ''
+    search_url = f'{request.base_url}api/v1/films/search'
+
+    # TODO: обработать ошибку если ничего не найдено
     async with aiohttp.ClientSession() as session:
-        async with session.get('http://python.org', json=params) as response:
+        # Поиск фильма по ключевым словам
+        async with session.get(search_url, params=search_film_params) as response:
+            film = await response.json()
+            film_id = film[0].get('id')
 
-            print("Status:", response.status)
-            print("Content-type:", response.headers['content-type'])
-
-            result = await response.text()
+        # Поик полной информации по фильму
+        film_information_url = f'{request.base_url}api/v1/films/{film_id}'
+        async with session.get(film_information_url) as response:
+            result = await response.json()
+            return result
 
--- async_api/src/api/v1/voice_command/comand.py
+++ async_api/src/api/v1/voice_command/comand.py
@@ -4,9 +4,15 @@ from pydantic import BaseConfig
 
 class TextCommand(BaseConfig):
     welcome: str = 'Добро пожаловать в навык "Практикум". Чем я могу Вам помочь?'
-    film: tuple = ('покажи фильм', 'что за фильм', )
-    author: tuple = ('автор фильма', )
-    error: str = 'Извените я не знаю как Вам помочь. Попробуйте еще раз!'
+    error: str = 'Извините я не знаю как Вам помочь. Попробуйте еще раз!'
     
+    film: tuple = ('найди фильм', 'покажи фильм', 'что за фильм', 'покажи мне фильм')
+    context_film_to_genre: tuple = ('жанр у этого фильма', )
+    context_film_to_decription: tuple = ('о чем этот фильм', 'о чем фильм', )
+    context_film_to_actors: tuple = ('снялся', 'кто снимался в фильме', )
+
+    author: tuple = ('автор фильма', )
+    
+    end: tuple = ('спасибо', )
 
 text_commands = TextCommand()

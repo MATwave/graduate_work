3389d50766d1e3b36112f02f87e4190ab4d7ea1c
kaluzhinas <kaluzhinas@gmail.com>
2023-05-17 21:43:49 +0700

    Get data by film in ES and Redis

--- async_api/src/api/v1/marusya_assistant.py
+++ async_api/src/api/v1/marusya_assistant.py
@@ -2,9 +2,10 @@ from enum import Enum
 import requests
 from core.config import settings
 from db import redis
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request
 from urllib.parse import urljoin, urlencode
 from .voice_command.comand import text_commands
+import aiohttp
 
 
 from .deserializers.marusya_deserializer import MarusyaRequestModel
@@ -14,12 +15,20 @@ router = APIRouter()
 
 
 
-@router.post("/marusya", response_model= MarusyaResponseModel)
-async def get_data_assistant(request: MarusyaRequestModel):
-    session_id = request.session.session_id
-    user_id = request.session.user_id
-    message_id = request.session.message_id
+@router.post("/marusya",response_model= MarusyaResponseModel)
 
+async def get_data_assistant(requestMarusya: MarusyaRequestModel,
+                             request: Request):
+    print(request.base_url)
+    session_id = requestMarusya.session.session_id
+    user_id = requestMarusya.session.user_id
+    message_id = requestMarusya.session.message_id
+
+    last_state = await redis.redis.get(session_id)
+
+    await redis.redis.setex(session_id,
+                            settings.cache_expires,
+                            str({'aaa':1}))
 
     resp = MarusyaResponseModel(
                 response=ResponseMarusya(
@@ -31,22 +40,41 @@ async def get_data_assistant(request: MarusyaRequestModel):
                     )
                 )
     
-    if request.session.new:
+
+    if requestMarusya.session.new:
         resp.response.text = text_commands.welcome 
     
-    if _check_command(request.request.command, text_commands.film):
+    if _check_command(requestMarusya.request.command, text_commands.film):
+        url = f'{request.base_url}/api/v1/films/search'
+        params = {
+            "page[size]": 1,
+            "page[number]": 1,
+            "sort": "-imdb_rating"
+        }
+        data_from_es = await _get_films(url=url, params=params)[0]
         resp.response.text = 'что то про фильм'
                 
-    if _check_command(request.request.command, text_commands.author):
-        resp.response.text = 'Автор Антонша'
-        print(request)
+    if _check_command(requestMarusya.request.command, text_commands.author):
+        resp.response.text = 'Автор'
+
                 
     return dict(resp)
 
 
 
-def _check_command(text_request: str, command: Enum) -> bool:
+def _check_command(text_request: str, command: tuple) -> bool:
     for phrase in command:
         if phrase in text_request:
             return True
         return False
+    
+
+async def _get_films(url: str, params: dict) -> dict:
+    async with aiohttp.ClientSession() as session:
+        async with session.get('http://python.org', json=params) as response:
+
+            print("Status:", response.status)
+            print("Content-type:", response.headers['content-type'])
+
+            result = await response.text()
+
--- async_api/src/api/v1/voice_command/comand.py
+++ async_api/src/api/v1/voice_command/comand.py
@@ -4,8 +4,8 @@ from pydantic import BaseConfig
 
 class TextCommand(BaseConfig):
     welcome: str = 'Добро пожаловать в навык "Практикум". Чем я могу Вам помочь?'
-    film: tuple = ('покажи фильм', 'что за фильм')
-    author: tuple = ('автор фильма')
+    film: tuple = ('покажи фильм', 'что за фильм', )
+    author: tuple = ('автор фильма', )
     error: str = 'Извените я не знаю как Вам помочь. Попробуйте еще раз!'
     
 

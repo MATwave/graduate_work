a9162c647f5b5bfe2ae4b3ff98a753a6c0119820
MATwave <stanislav.matveew@yandex.ru>
2023-05-21 04:47:11 +0300

    fix: починил передачу состояния

--- async_api/src/services/voice_assistent/alice.py
+++ async_api/src/services/voice_assistent/alice.py
@@ -43,10 +43,10 @@ class AliceService:
 
         elif any(intent.startswith("about_film_context") for intent in alice_request_model.request.nlu.intents):
             logger.info('определили интент about_film_context')
-            get_film_state = alice_request_model.state['session']['get_film'].get('film_data')
-            text = await self._context_answer_to_questions(state=get_film_state, request=alice_request_model)
+
+            text, new_state = await self._context_answer_to_questions(request=alice_request_model)
             response.set_text(text)
-            response.set_state(state_dict={'get_film': alice_request_model.state['session']})
+            response.set_state(state_dict={'get_film': new_state})
             response.set_buttons(text_commands.end[0])
             return response.dumps()
 
@@ -116,36 +116,37 @@ class AliceService:
 
         return msg, new_state
 
-    async def _context_answer_to_questions(self, state: dict, request: AliceRequestModel) -> tuple[str, dict]:
-        logger.info(state)
+    async def _context_answer_to_questions(self, request: AliceRequestModel) -> tuple[str, dict]:
 
         phrase: str = text_commands.error
-        new_state = state
+        new_state = request.state['session']
+        film_data = request.state['session']['get_film'].get('film_data')
 
         # Реакция на просьбу получить информацию о жанре в текущем фильме
         if 'about_film_context_genre' in request.request.nlu.intents:
             logger.info('определили интент about_genre')
-            phrase = ', '.join(state.get('genre'), text_commands.context_film_to_genre.error_response)
+            phrase = ', '.join(film_data.get('genre'), text_commands.context_film_to_genre.error_response)
 
         # Реакция на просьбу получить информацию об описании фильма
         if 'about_film_context_description' in request.request.nlu.intents:
             logger.info('определили интент about_description')
-            phrase = state.get('description', text_commands.context_film_to_decription.error_response)
+            phrase = film_data.get('description', text_commands.context_film_to_decription.error_response)
 
         # Реакция на просьбу получить информацию об актерах в фильме
         if 'about_film_context_actor' in request.request.nlu.intents:
             logger.info('определили интент about_actors')
-            if state.get('actors'):
-                phrase = ', '.join([c.get('name') for c in state.get('actors')])
+            if film_data.get('actors'):
+                phrase = ', '.join([c.get('name') for c in film_data.get('actors')])
             else:
                 phrase = text_commands.context_film_to_actors.error_response
 
         # Реакция на просьбу получить рекомендацию по фильму в таком же жанре
         if 'about_film_context_same_genre_film' in request.request.nlu.intents:
             logger.info('определили интент about_same_genre_film')
-            if state.get('genre'):
+            if film_data.get('genre'):
                 try:
-                    data_from_es, new_state = await self._get_random_films(genre=state.get('genre'))
+                    data_from_es, new_state = await self._get_random_films(genre=film_data.get('genre'))
+
                     phrase = data_from_es.title
                 except Exception as e:
                     logger.warning(e)

af63856963f47a87a368386c0975e34b4d9260be
kaluzhinas <kaluzhinas@gmail.com>
2023-05-23 22:35:13 +0700

    Test from Marusya

---
+++ assistant_handler/tests/marusya/request_examples/hello.py
@@ -0,0 +1,57 @@
+hello_request = {
+  "meta": {
+    "client_id": "MailRu-VC/1.0",
+    "locale": "ru_RU",
+    "timezone": "Asia/Barnaul",
+    "interfaces": {
+      "screen": {},
+      "audio_player": {}
+    },
+    "_city_ru": "Барнаул"
+  },
+  "request": {
+    "command": "привет",
+    "original_utterance": "привет",
+    "type": "SimpleUtterance",
+    "nlu": {
+      "tokens": [
+        "привет"
+      ],
+      "entities": []
+    }
+  },
+  "session": {
+    "session_id": "ea660359-9a97-49b3-a043-15546672ea48",
+    "user_id": "b17c7c4bb3278a07147c323fe3d6863cfd75f7a492da1358d92c7f7eaa859e53",
+    "skill_id": "52401ef1-935a-49ef-bd57-1f3f83ac3447",
+    "new": True,
+    "message_id": 0,
+    "user": {
+      "user_id": "af11d9c8-1683-4e40-bbff-a48b057cea6a"
+    },
+    "application": {
+      "application_id": "b17c7c4bb3278a07147c323fe3d6863cfd75f7a492da1358d92c7f7eaa859e53",
+      "application_type": "vk"
+    },
+    "auth_token": "636d5f7d6fb18e1aff48bb161ab82de12012fec213a0bf2e9a9efd8b7825c69c"
+  },
+  "state": {
+    "session": {},
+    "user": {}
+  },
+  "version": "1.0"
+}
+
+hello_response = {
+  "response": {
+    "text": "Добро пожаловать в навык \"Практикум\". Чем я могу Вам помочь?",
+    "end_session": False
+  },
+  "session": {
+    "session_id": "ea660359-9a97-49b3-a043-15546672ea48",
+    "user_id": "b17c7c4bb3278a07147c323fe3d6863cfd75f7a492da1358d92c7f7eaa859e53",
+    "message_id": 0
+  },
+  "session_state": {},
+  "version": "1.0"
+}
---
+++ assistant_handler/tests/marusya/test_marusya.py
@@ -0,0 +1,40 @@
+import pytest
+from src.models.marusya.response import MarusyaResponseModel
+from tests.marusya.request_examples import (hello,
+                                          get_film,
+                                          bye,
+                                          about_film_description,
+                                          about_film_actors,
+                                          get_same_genre_film,
+                                          misunderstanding)
+
+
+@pytest.mark.parametrize(
+    "request_data, expected_response",
+    [
+        (hello.hello_request, hello.hello_response),
+        (bye.bye_request, bye.bye_response),
+        (about_film_description.description_request, about_film_description.description_response),
+        (about_film_actors.actors_request, about_film_actors.actors_response),
+        (misunderstanding.misunderstanding_request, misunderstanding.misunderstanding_response)
+    ]
+)
+def test_get_data_assistant(test_client, request_data, expected_response):
+    response = test_client.post("api/v1/assistants/marusya", json=request_data)
+    assert response.status_code == 200
+    assert response.json() == expected_response
+
+
+@pytest.mark.parametrize(
+    "request_data",
+    [
+        get_film.request,
+        get_same_genre_film.request,
+    ]
+)
+def test_get_film(test_client, request_data):
+    response = test_client.post("api/v1/assistants/marusya", json=request_data)
+    assert response.status_code == 200
+    assert response.headers["Content-Type"] == "application/json"
+    # так как ответ каждый раз разный - проверим валидацию модели ответа
+    assert MarusyaResponseModel(**response.json())
